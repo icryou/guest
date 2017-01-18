55ab449b6048dc8776598961b5d82641bf1e613a
曾玥 <zengyue@cengyuedeMac-mini.local>
2017-01-18 19:39:43 +0800

    ADD: add pagination

--- sign/views.py
+++ sign/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from sign.models import Event, Guest
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -32,9 +33,19 @@ def login_action(request):
 # 发布会管理
 @login_required()
 def event_manage(request):
-    event_list = Event.objects.all()
     username = request.session.get('user', '')
-    return render(request, 'event_manage.html', {"user": username, "events": event_list})
+    event_list = Event.objects.all()
+    paginator = Paginator(event_list, 2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, 'event_manage.html', {"user": username, "events": contacts})
 
 
 # 发布会名称搜索
@@ -43,7 +54,17 @@ def sreach_name(request):
     username = request.session.get('user', '')
     sreach_name = request.GET.get('name', "")
     event_list = Event.objects.filter(name__contains=sreach_name)
-    return render(request, "event_manage.html", {"user": username, "events": event_list})
+    paginator = Paginator(event_list, 2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, "event_manage.html", {"user": username, "events": contacts})
 
 
 # 嘉宾管理
@@ -51,7 +72,17 @@ def sreach_name(request):
 def guest_manage(request):
     username = request.session.get('user', '')
     guest_list = Guest.objects.all()
-    return render(request, "guest_manage.html", {"user": username, "guests": guest_list})
+    paginator = Paginator(guest_list, 2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, "guest_manage.html", {"user": username, "guests": contacts})
 
 
 # 嘉宾手机号搜索
@@ -61,4 +92,14 @@ def sreach_phone(request):
     sreach_phone = request.GET.get('phone', "")
     sreach_name_bytes = sreach_phone.encode(encoding="utf-8")
     guest_list = Guest.objects.filter(phone__contains=sreach_name_bytes)
-    return render(request, "guest_manage.html", {"user": username, "guests": guest_list})
+    paginator = Paginator(guest_list, 2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, "guest_manage.html", {"user": username, "guests": contacts})
